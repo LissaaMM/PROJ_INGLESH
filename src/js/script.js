f20c64f1ce18c469e677d6ab1a8aff10be190ff3
Ana Larissa Mendes <152804250+LissaaMM@users.noreply.github.com>
2024-10-31 16:29:12 -0300

    bora

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
 function redirectPage(value) { 
     window.location.href = './src/html/' + value.toLowerCase() + '.html'; 
 }
+let score = 0; const buttons = document.querySelectorAll('.color-button'); buttons.forEach(button => { button.addEventListener('click', (e) => { if (e.target.closest('button').dataset.answer === 'correct') { e.target.closest('button').style.backgroundColor = 'green'; score++; } else { e.target.closest('button').style.backgroundColor = 'red'; } document.getElementById('score').innerText = 'Acertos: ' + score; }); 
+})

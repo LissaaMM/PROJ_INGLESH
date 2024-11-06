8e1616391b9b1c1467e68e8acbe15395158a143e
Ana Larissa Mendes <152804250+LissaaMM@users.noreply.github.com>
2024-11-06 10:31:07 -0300

    up

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,30 @@
-function redirectPage(value) { 
-    window.location.href = './src/html/' + value.toLowerCase() + '.html'; 
+function toggleSidebar() {
+    const sidebar = document.getElementById('navigation_header');
+    const isHidden = sidebar.style.marginLeft === '-100vw' || sidebar.style.marginLeft === '';
+
+    if (isHidden) {
+        sidebar.style.animation = 'showSidebar 0.3s forwards';
+        sidebar.style.marginLeft = '0';
+    } else {
+        sidebar.style.animation = 'showSidebar 0.3s reverse';
+        sidebar.style.marginLeft = '-100vw';
+    }
 }
-let score = 0; const buttons = document.querySelectorAll('.color-button'); buttons.forEach(button => { button.addEventListener('click', (e) => { if (e.target.closest('button').dataset.answer === 'correct') { e.target.closest('button').style.backgroundColor = 'green'; score++; } else { e.target.closest('button').style.backgroundColor = 'red'; } document.getElementById('score').innerText = 'Acertos: ' + score; }); 
-})
+
+function redirectPage(value) {
+    window.location.href = './src/html/' + value.toLowerCase() + '.html';
+}
+
+let score = 0;
+const buttons = document.querySelectorAll('.color-button');
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.closest('button').dataset.answer === 'correct') {
+            e.target.closest('button').style.backgroundColor = 'green';
+            score++;
+        } else {
+            e.target.closest('button').style.backgroundColor = 'red';
+        }
+        document.getElementById('score').innerText = 'Acertos: ' + score;
+    });
+});
